1ee2d368d10e4ac26aa0f491cee762f7cd15f92e
Sven Anderson <sven@anderson.de>
2025-07-12 16:57:30 +0200

    Remove unnecessary code and dependencies

--- tests/test_common.ml
+++ tests/test_common.ml
@@ -1,9 +1,3 @@
-open OUnit2
-
-let (prf, strf) = Format.(fprintf, asprintf)
-let pp_map pp f ppf x = pp ppf (f x)
-let pp_diff pp ppf (a, b) = prf ppf "@[<v>want: %a@,have: %a@]" pp a pp b
-
 let of_hex ?(skip_ws = true) s =
   let fold f acc str =
     let st = ref acc in
@@ -33,36 +27,3 @@ let of_hex ?(skip_ws = true) s =
   let chars = List.rev chars in
   assert (leftover = None);
   String.init (List.length chars) (fun i -> char_of_int (List.nth chars i))
-
-let rec range a b =
-  if a > b then [] else a :: range (succ a) b
-
-let rec times ~n f a =
-  if n > 0 then ( ignore (f a) ; times ~n:(pred n) f a )
-
-let pp_opt pp ppf = Format.(function
-  | Some x -> fprintf ppf "Some(%a)" pp x
-  | None   -> fprintf ppf "None")
-
-let eq_opt eq a b = match (a, b) with
-  | (Some x, Some y) -> eq x y
-  | _                -> false
-
-let pp_octets pp = pp (Ohex.pp_hexdump ())
-
-let assert_oct_equal ?msg =
-  assert_equal ~cmp:String.equal ?msg ~pp_diff:(pp_octets pp_diff)
-
-let iter_list xs f = List.iter f xs
-
-let cases_of f =
-  List.map @@ fun params -> test_case (f params)
-
-let any _ = true
-
-let vx = Ohex.decode
-
-let f1_eq ?msg f (a, b) _ =
-  assert_oct_equal ?msg (f (vx a)) (vx b)
-
-let f2_eq ?msg f (a, b, c) = f1_eq ?msg (f (vx a)) (b, c)
